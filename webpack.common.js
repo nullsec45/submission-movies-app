18dd45c914f61ce41929158f69c9ab7bd9be48f3
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-10-01 16:09:52 +0700

    tambah  style-loader

--- webpack.common.js
+++ webpack.common.js
@@ -17,6 +17,9 @@ module.exports = {
                     },
                     {
                         loader: 'css-loader'
+                    },
+                    {
+                        loader: 'style-loader'
                     }
                 ]
             }
