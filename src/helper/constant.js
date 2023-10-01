adbeb56a2d5efc4d4b2ec46b529366cb5c355035
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-10-01 16:13:58 +0700

    contant property

---
+++ src/helper/constant.js
@@ -0,0 +1,10 @@
+const BASE_URL = "https://api.themoviedb.org/3";
+const IMAGE_URL = "https://image.tmdb.org/t/p/original";
+const MAX_BODY_LENGTH = 200;
+const API_KEY = "a01d230efd2d61c4f90ef4606ae3f233";
+export {
+    BASE_URL,
+    IMAGE_URL,
+    MAX_BODY_LENGTH,
+    API_KEY
+}
