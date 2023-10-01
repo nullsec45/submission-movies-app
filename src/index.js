0aabaf031f8d4162be8572e278ee4afe97801dab
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-10-01 16:11:55 +0700

    import app and component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
-import "./components/Navbar.js"
 import './styles/style.css';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import "./components/Title.js"
+import "./components/Button.js"
+import App from "./app.js";
+
+new App();
