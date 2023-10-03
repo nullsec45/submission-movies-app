29af016a9c20effc05c45facd9e2227dd3a452e7
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-10-03 22:58:30 +0700

    add space betwwen import component and class App

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import './styles/style.css';
 import "./components/Title.js"
 import "./components/Button.js"
+
 import App from "./app.js";
 
 new App();
