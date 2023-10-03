9a7c3fed0f49eeb74e1afd0dce8e28b7bbd295a4
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-10-03 22:56:30 +0700

    import evente search and navbarActive

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 import Movies from "./data/movies";
+import Event from "./events/event";
 
 class App {
     constructor() {
         Movies.trending();
         Movies.popular();
+        Event.searchEvent();
+        Event.navbarActive();
     }
 }
 
