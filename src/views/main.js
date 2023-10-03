a8e9308e54a1898feaacaf7928c75090b3f6995f
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-10-03 23:00:52 +0700

    searchView, validartionError

--- src/views/main.js
+++ src/views/main.js
@@ -1,5 +1,7 @@
 import { IMAGE_URL, MAX_BODY_LENGTH } from "../helper/constant.js";
 import { maxBody } from "../helper/max-body.js";
+import { alertError } from "../helper/alert.js";
+
 
 
 class View {
@@ -33,6 +35,36 @@ class View {
                         <div class="card-body">
                             <h5 class="card-title">${popular.title}</h5>
                             <p class="card-text">${maxBody(popular.overview, MAX_BODY_LENGTH)}</p>
+                        </div>
+                        <div class="card-footer text=center">
+                            <a href="#" class="btn btn-primary text-center d-flex justify-content-center">Show More</a>
+                        </div>
+                    </div>
+                </div>
+            `);
+        })
+    }
+
+    static searchView(data, titleInput) {
+        const searchCard = document.getElementById("search-card-container");
+        searchCard.innerHTML = "";
+        if (titleInput.value.trim("") == "") {
+            alertError("Empty Keyword or Title", "Please enter a keyword or film title, so we can find the film you want to watch.");
+            return
+        }
+        if (data.total_results == 0) {
+            alertError("Movie Not Found", "We can't find the film you are looking for");
+            titleInput.value = "";
+        }
+
+        data.results.forEach(movie => {
+            searchCard.insertAdjacentHTML('beforeend', `
+            <div class="col-lg-4 mt-4">
+                    <div class="card">
+                        <img src="${IMAGE_URL}${movie.poster_path}" class="card-img-top" alt="${movie.poster_path}">
+                        <div class="card-body">
+                            <h5 class="card-title">${movie.title}</h5>
+                            <p class="card-text">${maxBody(movie.overview, MAX_BODY_LENGTH)}</p>
                         </div>
                         <div class="card-footer text=center">
                             <a href="#" class="btn btn-primary text-center d-flex justify-content-center">Show More</a>
