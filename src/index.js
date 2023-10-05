60c8a76e5639641b805b222db963eb22c268fdec
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-10-06 06:29:54 +0700

    index.js:import bootstrap

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import './styles/style.css';
 import "./components/Title.js"
-import "./components/Button.js"
+import "./components/Button.js";
+import 'bootstrap';
 
 import App from "./app.js";
 
